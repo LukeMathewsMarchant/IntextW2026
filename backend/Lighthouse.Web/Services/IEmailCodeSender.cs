4e65749eb37b1f03bf4e45ef4c6f891063a20f39
Luke Marchant <gswebmaster@byu.edu>
2026-04-08 14:12:50 -0600

    Create ML-service-only deployment branch

--- backend/Lighthouse.Web/Services/IEmailCodeSender.cs
+++
@@ -1,7 +0,0 @@
-namespace Lighthouse.Web.Services;
-
-public interface IEmailCodeSender
-{
-    bool IsConfigured { get; }
-    Task SendTwoFactorCodeAsync(string toEmail, string code, CancellationToken cancellationToken = default);
-}
--- backend/Lighthouse.Web/Services/IEmailTwoFactorCodeStore.cs
+++
@@ -1,9 +0,0 @@
-namespace Lighthouse.Web.Services;
-
-public interface IEmailTwoFactorCodeStore
-{
-    string CreateChallenge(string userId);
-    bool TryGetUserId(string challengeId, out string userId);
-    string CreateCode(string challengeId);
-    bool ValidateCode(string challengeId, string code);
-}
